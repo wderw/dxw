1c579b872e7a4cf4ea5e65d03813aba2c5b337ef
wojciechderwisz@gmail.com <wojciechderwisz@gmail.com>
2024-11-01 15:17:17 +0100

    improve logging with file and line

--- dxw/Log.h
+++ dxw/Log.h
@@ -1,8 +1,15 @@
 #pragma once
 
 #include <memory>
+#include <string_view>
 
 #include <spdlog/spdlog.h>
+
+inline const char* extract_base_name(const char* full_path)
+{
+    const char* base_name = strrchr(full_path, '\\');
+    return base_name ? base_name + 1 : full_path;
+}
 
 namespace dxw
 {
@@ -22,9 +29,14 @@ private:
 
 }
 
-#define LOG_TRACE(...)    ::dxw::Log::GetLogger()->trace(__VA_ARGS__)
-#define LOG_INFO(...)     ::dxw::Log::GetLogger()->info(__VA_ARGS__)
-#define LOG_WARN(...)     ::dxw::Log::GetLogger()->warn(__VA_ARGS__)
-#define LOG_ERROR(...)    ::dxw::Log::GetLogger()->error(__VA_ARGS__)
-#define LOG_CRITICAL(...) ::dxw::Log::GetLogger()->critical(__VA_ARGS__)
+#define LOG_TRACE(format, ...) \
+    ::dxw::Log::GetLogger()->trace("[{}:{}]: " format, extract_base_name(__FILE__), __LINE__, ##__VA_ARGS__)
+#define LOG_INFO(format, ...) \
+    ::dxw::Log::GetLogger()->info("[{}:{}]: " format, extract_base_name(__FILE__), __LINE__, ##__VA_ARGS__)
+#define LOG_WARN(format, ...) \
+    ::dxw::Log::GetLogger()->warn("[{}:{}]: " format, extract_base_name(__FILE__), __LINE__, ##__VA_ARGS__)
+#define LOG_ERROR(format, ...) \
+    ::dxw::Log::GetLogger()->error("[{}:{}]: " format, extract_base_name(__FILE__), __LINE__, ##__VA_ARGS__)
+#define LOG_CRITICAL(format, ...) \
+    ::dxw::Log::GetLogger()->critical("[{}:{}]: " format, extract_base_name(__FILE__), __LINE__, ##__VA_ARGS__)
 
