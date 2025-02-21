16c31703f98ca7542f6cd80c1faa285163b856a1
wojciechderwisz@gmail.com <wojciechderwisz@gmail.com>
2025-02-21 19:16:26 +0100

    add define for dllexport

--- dxw/dxw_interface.h
+++ dxw/dxw_interface.h
@@ -2,34 +2,38 @@
 
 #include <windows.h>
 
+#define DXW_INTERFACE_API extern "C" __declspec(dllexport)
+
 namespace dxw
 {
 // 3d-specific
-extern "C" __declspec(dllexport) void DXW_D3D_Clear(float, float, float, float);
+DXW_INTERFACE_API void DXW_D3D_Clear(float, float, float, float);
 
 // 2d-specific
-extern "C" __declspec(dllexport) void DXW_D2D_BeginDraw();
-extern "C" __declspec(dllexport) void DXW_D2D_EndDraw();
-extern "C" __declspec(dllexport) void DXW_D2D_Clear(float, float, float, float);
-extern "C" __declspec(dllexport) void DXW_D2D_DrawLine(float, float, float, float);
-extern "C" __declspec(dllexport) void DXW_D2D_SetScale(float, float);
-extern "C" __declspec(dllexport) void DXW_D2D_SetRotation(float);
-extern "C" __declspec(dllexport) void DXW_D2D_SetTranslation(float, float);
-extern "C" __declspec(dllexport) void DXW_D2D_RecalculateTransformMatrix();
-extern "C" __declspec(dllexport) void DXW_D2D_ResetTransformMatrix();
-extern "C" __declspec(dllexport) void DXW_D2D_DrawText(const WCHAR * string, float left, float top, float right, float bottom);
+DXW_INTERFACE_API void DXW_D2D_BeginDraw();
+DXW_INTERFACE_API void DXW_D2D_EndDraw();
+DXW_INTERFACE_API void DXW_D2D_Clear(float, float, float, float);
+DXW_INTERFACE_API void DXW_D2D_DrawLine(float, float, float, float);
+DXW_INTERFACE_API void DXW_D2D_SetScale(float, float);
+DXW_INTERFACE_API void DXW_D2D_SetRotation(float);
+DXW_INTERFACE_API void DXW_D2D_SetTranslation(float, float);
+DXW_INTERFACE_API void DXW_D2D_RecalculateTransformMatrix();
+DXW_INTERFACE_API void DXW_D2D_ResetTransformMatrix();
+DXW_INTERFACE_API void DXW_D2D_DrawText(const WCHAR * string, float left, float top, float right, float bottom);
 
 // dx-general
-extern "C" __declspec(dllexport) void DXW_Present(int);
-extern "C" __declspec(dllexport) bool DXW_IsInitialized();
-extern "C" __declspec(dllexport) void DXW_ReleaseDxwResources();
-extern "C" __declspec(dllexport) int  DXW_InitWindow(HWND hWnd);
-extern "C" __declspec(dllexport) void DXW_ResizeWindow(unsigned int, unsigned int);
-extern "C" __declspec(dllexport) void DXW_SetTargetWindow(int);
+DXW_INTERFACE_API void DXW_Present(int);
+DXW_INTERFACE_API bool DXW_IsInitialized();
+DXW_INTERFACE_API void DXW_ReleaseDxwResources();
+DXW_INTERFACE_API int  DXW_InitWindow(HWND hWnd);
+DXW_INTERFACE_API void DXW_ResizeWindow(unsigned int, unsigned int);
+DXW_INTERFACE_API void DXW_SetTargetWindow(int);
 
 // demos
-extern "C" __declspec(dllexport) void DXW_DemoNRT(float);
-extern "C" __declspec(dllexport) void DXW_DemoLines(int);
-extern "C" __declspec(dllexport) void DXW_Demo3D();
+DXW_INTERFACE_API void DXW_DemoNRT(float);
+DXW_INTERFACE_API void DXW_DemoLines(int);
+DXW_INTERFACE_API void DXW_Demo3D();
 
 }
+
+#undef DXW_INTERFACE_API
